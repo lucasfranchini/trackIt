184e2603f2564da9f852b22470a249c0e03b60cc
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 16:10:12 -0300

    style: format habits.js

--- src/Components/Habits/Habits.js
+++ src/Components/Habits/Habits.js
@@ -7,82 +7,111 @@ import NewHabit from "./NewHabit";
 import TodayContext from "../../contexts/TodayContext";
 import Body from "../../styles/PageBody";
 
-export default function Habits(){
-    const {user} = useContext(UserContext);
-    const [newHabit,setNewHabit] = useState(false);
-    const [habits,setHabits] = useState([])
-    const token = user===null ? "":user.token;
-    const {today,setToday} = useContext(TodayContext);
-    const [habit,setHabit] = useState({
-        name: "",
-        days:[]
+export default function Habits() {
+  const { user } = useContext(UserContext);
+  const [newHabit, setNewHabit] = useState(false);
+  const [habits, setHabits] = useState([]);
+  const token = user === null ? "" : user.token;
+  const { today, setToday } = useContext(TodayContext);
+  const [habit, setHabit] = useState({
+    name: "",
+    days: [],
+  });
+
+  useEffect(() => {
+    if (token !== "") {
+      const promise = axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/habits`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      promise.then((answer) => setHabits(answer.data));
+      promise.catch((e) => alert("houve um erro ao pegar seus habitos"));
+    }
+  }, [token]);
+
+  if (user === null) return null;
+
+  function deleteHabit(habit) {
+    if (window.confirm("tem certeza que deseja deletar esse habito?")) {
+      const promise = axios.delete(
+        `${process.env.REACT_APP_API_BASE_URL}/habits/${habit.id}`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      promise.then(() => {
+        setHabits(habits.filter((h) => habit.id !== h.id));
+        setToday(today.filter((h) => habit.id !== h.id));
+      });
+      promise.catch((e) => alert("houve um erro ao apagar seus habitos"));
+    }
+  }
+
+  function addHabit(habit) {
+    setHabits([...habits, habit]);
+    setNewHabit(false);
+    setHabit({
+      name: "",
+      days: [],
     });
-
-    useEffect(()=>{
-        if(token !== ""){
-            const promise = axios.get(`${process.env.REACT_APP_API_BASE_URL}/habits`,{headers:{Authorization: `Bearer ${token}`}})
-            promise.then(answer=>setHabits(answer.data));
-            promise.catch((e)=>alert("houve um erro ao pegar seus habitos"));
-        }
-    },[token]);
-
-    if(user===null) return null;
-
-    function deleteHabit(habit){
-        if(window.confirm("tem certeza que deseja deletar esse habito?")){
-            const promise = axios.delete(`${process.env.REACT_APP_API_BASE_URL}/habits/${habit.id}`,{headers:{Authorization: `Bearer ${token}`}});
-            promise.then(()=>{
-                setHabits(habits.filter(h=>habit.id!==h.id));
-                setToday(today.filter(h=>habit.id!==h.id));
-            });
-            promise.catch((e)=>alert("houve um erro ao apagar seus habitos"));
-        }
-    }
-
-    function addHabit(habit){
-        setHabits([...habits,habit]);
-        setNewHabit(false);
-        setHabit({
-            name: "",
-            days:[]
-        });
-        const promise = axios.get(`${process.env.REACT_APP_API_BASE_URL}/habits/today`,{headers:{Authorization: `Bearer ${token}`}});
-        promise.then(answer=>setToday(answer.data));
-        promise.catch((e)=>alert("houve um erro ao adicionar seu habito"));
-    }
-
-    return (
-
-        <Body>
-            <Title>
-                <span>Meus hábitos</span>
-                <Button onClick={()=>setNewHabit(true)}>+</Button>
-            </Title>
-            {newHabit && <NewHabit addHabit={addHabit} setNewHabit={setNewHabit} habit={habit} setHabit={setHabit}/>}
-            { habits.length===0 && <div>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</div>}
-            { habits.length!==0 && habits.map(habit=><Habit key={habit.id} habit={habit} deleteHabit={deleteHabit} done={null} days={habit.days}/>)}
-        </Body>
+    const promise = axios.get(
+      `${process.env.REACT_APP_API_BASE_URL}/habits/today`,
+      { headers: { Authorization: `Bearer ${token}` } }
     );
+    promise.then((answer) => setToday(answer.data));
+    promise.catch((e) => alert("houve um erro ao adicionar seu habito"));
+  }
+
+  return (
+    <Body>
+      <Title>
+        <span>Meus hábitos</span>
+        <Button onClick={() => setNewHabit(true)}>+</Button>
+      </Title>
+      {newHabit && (
+        <NewHabit
+          addHabit={addHabit}
+          setNewHabit={setNewHabit}
+          habit={habit}
+          setHabit={setHabit}
+        />
+      )}
+      {habits.length === 0 && (
+        <div>
+          Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para
+          começar a trackear!
+        </div>
+      )}
+      {habits.length !== 0 &&
+        habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            deleteHabit={deleteHabit}
+            done={null}
+            days={habit.days}
+          />
+        ))}
+    </Body>
+  );
 }
 const Title = styled.div`
-    width:100%;
-    display:flex;
-    align-items:center;
-    justify-content:space-between;
-    padding-top:22px;
-    font-size: 23px;
-    line-height: 29px;
-    color:#126BA5;
-    margin-bottom:28px;
-`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 22px;
+  font-size: 23px;
+  line-height: 29px;
+  color: #126ba5;
+  margin-bottom: 28px;
+`;
 const Button = styled.button`
-    width: 40px;
-    height: 35px;
-    background:#52B6FF;
-    border:none;
-    border-radius: 5px;
-    color:#fff;
-    font-size: 27px;
-    line-height:33px;
-    text-indent: 1px;
-`
+  width: 40px;
+  height: 35px;
+  background: #52b6ff;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 27px;
+  line-height: 33px;
+  text-indent: 1px;
+`;
--- src/Components/Habits/NewHabit.js
+++ src/Components/Habits/NewHabit.js
@@ -6,71 +6,101 @@ import Input from "../../styles/Input";
 import Week from "../Aux/Week";
 import Loader from "react-loader-spinner";
 
-export default function NewHabit({addHabit,setNewHabit,setHabit,habit}){
-    const {user} = useContext(UserContext);
-    const [load,setLoad] = useState(false);
-    function createHabit(){
-        setLoad(true);
-        const promise = axios.post(`${process.env.REACT_APP_API_BASE_URL}/habits`,habit,{headers:{Authorization: `Bearer ${user.token}`}});
-        promise.then(answer=>addHabit(answer.data));
-        promise.catch(()=>{
-            setLoad(false);
-            alert("Houve um erro ao tentar criar um habito");
-        });
-    }
-    function cancel(){
-        setNewHabit(false);
-        setHabit({...habit});
-    }
-    return (
-        <Body>
-            <Input placeholder="nome do hábito" type="email" value={habit.name} onChange={e=>setHabit({...habit,name: e.target.value})} disabled={load}/>
-            <Week habit={habit} setHabit={setHabit} changeable={true} days={habit.days} done={null}/>
-            <Buttons >
-                <Cancelar disabled={load} onClick={cancel}>Cancelar</Cancelar>
-                <Salvar disabled={load} onClick={createHabit}>{load ? <Loader type="ThreeDots" color="#FFF" height={50} width={50} radius={0}/>:"Salvar"}</Salvar>
-            </Buttons>
-        </Body>
+export default function NewHabit({ addHabit, setNewHabit, setHabit, habit }) {
+  const { user } = useContext(UserContext);
+  const [load, setLoad] = useState(false);
+  function createHabit() {
+    setLoad(true);
+    const promise = axios.post(
+      `${process.env.REACT_APP_API_BASE_URL}/habits`,
+      habit,
+      { headers: { Authorization: `Bearer ${user.token}` } }
     );
+    promise.then((answer) => addHabit(answer.data));
+    promise.catch(() => {
+      setLoad(false);
+      alert("Houve um erro ao tentar criar um habito");
+    });
+  }
+  function cancel() {
+    setNewHabit(false);
+    setHabit({ ...habit });
+  }
+  return (
+    <Body>
+      <Input
+        placeholder="nome do hábito"
+        type="email"
+        value={habit.name}
+        onChange={(e) => setHabit({ ...habit, name: e.target.value })}
+        disabled={load}
+      />
+      <Week
+        habit={habit}
+        setHabit={setHabit}
+        changeable={true}
+        days={habit.days}
+        done={null}
+      />
+      <Buttons>
+        <Cancelar disabled={load} onClick={cancel}>
+          Cancelar
+        </Cancelar>
+        <Salvar disabled={load} onClick={createHabit}>
+          {load ? (
+            <Loader
+              type="ThreeDots"
+              color="#FFF"
+              height={50}
+              width={50}
+              radius={0}
+            />
+          ) : (
+            "Salvar"
+          )}
+        </Salvar>
+      </Buttons>
+    </Body>
+  );
 }
 
-const Body =styled.div`
-    width:100%;
-    background:#fff;
-    padding:18px;
-    margin-bottom:30px;
-`
+const Body = styled.div`
+  width: 100%;
+  background: #fff;
+  padding: 18px;
+  margin-bottom: 30px;
+`;
 const Buttons = styled.div`
-    width:100%;
-    display:flex;
-    justify-content:flex-end;
-    margin-top:30px;
-`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 30px;
+`;
 const Cancelar = styled.button`
-    width: 84px;
-    height: 35px;
-    color:#52B6FF;
-    font-size:16px;
-    line-height:20px;
-    text-align:center;
-    border:none;
-    background:inherit;
-    margin-right:23px;
-    opacity: ${props => props.disabled ? 0.7:1};
-`
+  width: 84px;
+  height: 35px;
+  color: #52b6ff;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  border: none;
+  background: inherit;
+  margin-right: 23px;
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+`;
 
 const Salvar = styled.button`
-    width: 84px;
-    height: 35px;
-    background: #52B6FF;
-    border-radius: 5px;
-    color: #fff;
-    border: none;
-    font-size:16px;
-    line-height:20px;
-    text-align:center;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    opacity: ${props => props.disabled ? 0.7:1};
-`
+  width: 84px;
+  height: 35px;
+  background: #52b6ff;
+  border-radius: 5px;
+  color: #fff;
+  border: none;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+`;
