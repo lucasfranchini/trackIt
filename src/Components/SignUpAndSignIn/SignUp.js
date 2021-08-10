4fca07db11d32c37fc21b22682152f786c2ff0c0
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 16:14:30 -0300

    style: Format SignUp.js

--- src/Components/SignUpAndSignIn/SignUp.js
+++ src/Components/SignUpAndSignIn/SignUp.js
@@ -7,70 +7,106 @@ import { useState } from "react";
 import axios from "axios";
 import Loader from "react-loader-spinner";
 
-export default function SignUp(){
-    const [body,setBody] = useState({
-            email: "",
-            name: "",
-            image: "",
-            password: ""
-        }); 
-    let history =  useHistory();
-    const [load,setLoad] = useState(false);
-    function register(e){
-        e.preventDefault();
-        setLoad(true)
-        const promise =  axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/sign-up`,body);
-        promise.then(()=>history.push("/"));
-        promise.catch(()=>{
-            alert("houve algum erro no seu registro, por favor tente novamente");
-            setLoad(false);
-        });
-    }
-    return (
-        <Body>
-            <Img src={logo} alt="TrackIt"/>
-            <form onSubmit={register}>
-                <Input type="email" placeholder="email" onChange={e=>setBody({...body,email: e.target.value})} value={body.email} disabled={load} required/>
-                <Input type="password" placeholder="senha" onChange={e=>setBody({...body,password: e.target.value})} value={body.password} disabled={load} required/>
-                <Input type="text" placeholder="nome" onChange={e=>setBody({...body,name: e.target.value})} value={body.name} disabled={load} required/>
-                <Input type="url" placeholder="foto" onChange={e=>setBody({...body,image: e.target.value})} value={body.image} disabled={load} required/>
-                <Button disabled={load} type="submit">{load ? <Loader type="ThreeDots" color="#FFF" height={50} width={50}/>:"Cadastrar"}</Button>
-            </form>
-            <Link to="/">
-                <span>Já tem uma conta? Faça login!</span>
-            </Link>
-        </Body>
+export default function SignUp() {
+  const [body, setBody] = useState({
+    email: "",
+    name: "",
+    image: "",
+    password: "",
+  });
+  let history = useHistory();
+  const [load, setLoad] = useState(false);
+  function register(e) {
+    e.preventDefault();
+    setLoad(true);
+    const promise = axios.post(
+      `${process.env.REACT_APP_API_BASE_URL}/auth/sign-up`,
+      body
     );
+    promise.then(() => history.push("/"));
+    promise.catch(() => {
+      alert("houve algum erro no seu registro, por favor tente novamente");
+      setLoad(false);
+    });
+  }
+  return (
+    <Body>
+      <Img src={logo} alt="TrackIt" />
+      <form onSubmit={register}>
+        <Input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setBody({ ...body, email: e.target.value })}
+          value={body.email}
+          disabled={load}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="senha"
+          onChange={(e) => setBody({ ...body, password: e.target.value })}
+          value={body.password}
+          disabled={load}
+          required
+        />
+        <Input
+          type="text"
+          placeholder="nome"
+          onChange={(e) => setBody({ ...body, name: e.target.value })}
+          value={body.name}
+          disabled={load}
+          required
+        />
+        <Input
+          type="url"
+          placeholder="foto"
+          onChange={(e) => setBody({ ...body, image: e.target.value })}
+          value={body.image}
+          disabled={load}
+          required
+        />
+        <Button disabled={load} type="submit">
+          {load ? (
+            <Loader type="ThreeDots" color="#FFF" height={50} width={50} />
+          ) : (
+            "Cadastrar"
+          )}
+        </Button>
+      </form>
+      <Link to="/">
+        <span>Já tem uma conta? Faça login!</span>
+      </Link>
+    </Body>
+  );
 }
 
 const Img = styled.img`
-    margin-bottom:25px;
-`
+  margin-bottom: 25px;
+`;
 
 const Body = styled.div`
-    display: flex;
-    justify-content:center;
-    align-items:center;
-    flex-direction: column;
-    width: 100%;
-    padding: 68px 36px;
-    color:#52B6FF;
-    font-size: 14px;
-    line-height: 17px;
-    text-align: center;
-    a:visited {
-        color: #52B6FF;
-        }
-
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  padding: 68px 36px;
+  color: #52b6ff;
+  font-size: 14px;
+  line-height: 17px;
+  text-align: center;
+  a:visited {
+    color: #52b6ff;
+  }
+`;
 const Button = styled.button`
-    width: 100%;
-    height:45px;
-    background: #52B6FF;
-    border-radius: 5px;
-    color: #fff;
-    border: none;
-    font-size: 20px;
-    margin-bottom: 25px;
-    opacity: ${props => props.disabled ? 0.7:1};
-`
+  width: 100%;
+  height: 45px;
+  background: #52b6ff;
+  border-radius: 5px;
+  color: #fff;
+  border: none;
+  font-size: 20px;
+  margin-bottom: 25px;
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+`;
