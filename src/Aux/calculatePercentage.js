b750bf30fb7ce42136d448c8fab3218c6b0172cd
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 16:16:02 -0300

    style: Format calculatePercentage.js

--- src/Aux/calculatePercentage.js
+++ src/Aux/calculatePercentage.js
@@ -1,15 +1,14 @@
-export default function calculatePercentage(setPercentage,today){
-    let newpercentage=0;
-    for(let i=0; i<today.length;i++){
-        
-        if(today[i].done){
-            newpercentage += 100/today.length;
-        }
-        if(i+1===today.length){
-            setPercentage(newpercentage);
-        }
+export default function calculatePercentage(setPercentage, today) {
+  let newpercentage = 0;
+  for (let i = 0; i < today.length; i++) {
+    if (today[i].done) {
+      newpercentage += 100 / today.length;
     }
-    if(today.length===0){
-        setPercentage(100);
-    }  
+    if (i + 1 === today.length) {
+      setPercentage(newpercentage);
+    }
+  }
+  if (today.length === 0) {
+    setPercentage(100);
+  }
 }
